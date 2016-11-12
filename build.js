6def7340560ea8686511698a8b54d6bcac09b9fc
David Wollschlegel <david@wollschlegel.com>
2016-11-12 14:53:43 +0100

    included photoswipe files directly in the css file
    as base64

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var pkg     = require('./package.json');
 var path    = require('path');
+var fs      = require('fs');
 var Builder = require('systemjs-builder');
 
 var builder = new Builder();
@@ -22,14 +23,32 @@ var config = {
   meta: {
     'node_modules/@angular/*': { build: false },
     'node_modules/rxjs/*': { build: false }
-  },
+  }
 };
 
+var photoswipeImages = [];
+photoswipeImages.push({mimeType: 'image/svg+xml', imgName: 'default-skin.png', replaceWith: ""});
+photoswipeImages.push({mimeType: 'image/png', imgName: 'default-skin.svg', replaceWith: ""});
+photoswipeImages.push({mimeType: 'image/gif', imgName: 'preloader.gif', replaceWith: ""});
+var photoswipeDefaultSkin = fs.readFileSync('./node_modules/photoswipe/dist/default-skin/default-skin.css', 'utf-8');
+photoswipeImages.forEach(function (img, index, array) {
+  var data = fs.readFileSync('./node_modules/photoswipe/dist/default-skin/' + img.imgName, "utf8");
+  var base64data = new Buffer(data).toString('base64');
+  var regEx = new RegExp(img.imgName, 'gm');
+  photoswipeDefaultSkin = photoswipeDefaultSkin.replace(regEx, "data:"+img.mimeType+";base64,"+base64data);
+  if (index === array.length - 1) {
+    fs.writeFile('./default-skin.css', photoswipeDefaultSkin);
+  }
+});
+
+
+
+
 builder.config(config);
-builder.buildStatic('photoswipe', 'photoswipe.js', {
+builder.buildStatic('photoswipe', {
   globalName: 'PhotoSwipe'
 });
-builder.buildStatic('photoswipeui', 'photoswipeui.js', {
+builder.buildStatic('photoswipeui', {
   globalName: 'PhotoSwipeUI_Default'
 });
 builder
