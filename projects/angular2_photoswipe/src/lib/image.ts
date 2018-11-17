c9f4dd92bf745192b96f64997fc125f6832133de
Vladimír Preclík <vladimir.preclik@intax.cz>
2018-11-17 18:48:57 +0100

    + allow html swipes, specify thumbnail size

--- projects/angular2_photoswipe/src/lib/image.ts
+++ projects/angular2_photoswipe/src/lib/image.ts
@@ -3,10 +3,17 @@ export class Image {
   index: number;
   pid: string;
   largeUrl: string;
+
   thumbUrl: string;
+  thumbWidth: number;
+  thumbHeight: number;
+
   size: string;
   width: number;
   height: number;
+
+  html: string;
+
   description: string;
   author: string;
 
