46a5043c2090158363ec9e56be1ec1b6d4ec1682
PuuTheBear <58558820+PuuTheBear@users.noreply.github.com>
2019-04-08 13:29:51 +0200

    LightboxOptions
    LightboxOptions

--- projects/angular2_photoswipe/src/lib/lightbox-options.ts
+++ projects/angular2_photoswipe/src/lib/lightbox-options.ts
@@ -1,1 +1,12 @@
-
+interface LightboxOptions {
+  showHideTransitionDuration: number;
+  controlsTransitionDuration: number;
+  opacity: number;
+  backgroundColor: string;
+  placeholderColor: string;
+  boxSizingBorderBox: boolean; // disable .pswp * { box-sizing:border-box } (in case you already have it in your site css)
+  rootZIndex: number;
+  assetsPath: string; //path to skin assets folder (preloader, PNG and SVG sprite)
+  ErrorTextColor: string; // "Image not loaded" text color
+  includeMinimalStyle: boolean;
+}
