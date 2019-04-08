225b70601c2b14ab6eebd13689abbb21083eab85
PuuTheBear <58558820+PuuTheBear@users.noreply.github.com>
2019-04-08 14:25:42 +0200

    LightxoxOptionsService
    LightxoxOptionsService

---
+++ projects/angular2_photoswipe/src/lib/lightbox-options-service.ts
@@ -0,0 +1,10 @@
+import { LightboxOptions } from './lightbox-options.ts'
+
+class LightxoxOptionsService {
+  constructor(readonly lightboxOptions: LightboxOptions) {}
+}
+ 
+const LIGHTBOX_OPTIONS = new InjectionToken<LightBoxOptionsService>('LightboxOptions', {
+  providedIn: 'root',
+  factory: () => new LightboxOptionsService(inject(MyDep)),
+});
