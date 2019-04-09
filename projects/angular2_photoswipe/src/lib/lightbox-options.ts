11d078541a27d44a02e040e1c1c5bff93772f9cf
Severin <severin.putzi@gmail.com>
2019-04-09 10:57:28 +0200

    Update lightbox-options.ts

--- projects/angular2_photoswipe/src/lib/lightbox-options.ts
+++ projects/angular2_photoswipe/src/lib/lightbox-options.ts
@@ -1,12 +1,19 @@
 export interface LightboxOptions {
-  showHideTransitionDuration: number;
-  controlsTransitionDuration: number;
-  opacity: number;
-  backgroundColor: string;
-  placeholderColor: string;
-  boxSizingBorderBox: boolean; // disable .pswp * { box-sizing:border-box } (in case you already have it in your site css)
-  rootZIndex: number;
-  assetsPath: string; //path to skin assets folder (preloader, PNG and SVG sprite)
-  ErrorTextColor: string; // "Image not loaded" text color
-  includeMinimalStyle: boolean;
+  allowPanToNext: boolean;
+	spacing: number;
+	bgOpacity: number;
+	mouseUsed: boolean;
+	loop: boolean;
+	pinchToClose: boolean;
+	closeOnScroll: boolean;
+	closeOnVerticalDrag: boolean;
+	verticalDragRange: number;
+	hideAnimationDuration: number;
+	showAnimationDuration: number;
+	showHideOpacity: boolean;
+	focus: boolean;
+	escKey: boolean;
+	arrowKeys: boolean;
+	mainScrollEndFriction: number;
+	panEndFriction: number;
 }
